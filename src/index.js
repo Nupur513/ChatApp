32219d12d6ed6adfce3180459d0c72a82bb86197
Nupur513 <nupur.panwar513@gmail.com>
2025-02-03 13:18:51 +0530

    socket implemented

--- src/index.js
+++ src/index.js
@@ -1,26 +1,42 @@
 import express from "express";
 import dotenv from "dotenv";
-import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
 dotenv.config();
 
-import authRoute from "./routes/auth.route.js";
-import messageRoute from "./routes/message.route.js";
-const app=express();
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use("/api/auth",authRoute)
-app.use("/api/message",messageRoute)
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true,
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
-));
-const PORT=process.env.PORT;
-app.listen(PORT,()=>{
-    console.log("server listening on port:" + PORT );
-    connectDB();
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
